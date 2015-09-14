08810e699ea03498bbbaba10e7d21bd45435318e
Mohit Patil <bravoure546@gmail.com>
2015-09-14 13:02:13 +0530

    test categories added

---
+++ onlineaptitude/static/js/app.js
@@ -0,0 +1,15 @@
+angular.module('TrackingApp', ['TrackingApp.controllers','ui.bootstrap','ui.router','TrackingApp.services','ngMap'])
+.config(function($stateProvider, $urlRouterProvider) {
+
+	// $urlRouterProvider.otherwise('/device/table');
+
+ //    $stateProvider
+ //    .state('home', {
+ //        url: '/home'
+ //    })
+ //    .state('addDevices', {
+ //        url: '/add/devices',
+ //        templateUrl: '/static/views/templates/addDevices.html',
+ //        controller: 'DeviceCtrl'
+ //    });
+});
---
+++ onlineaptitude/static/js/controllers.js
@@ -0,0 +1,10 @@
+angular.module('TrackingApp.controllers',[])
+.controller('MasterCtrl', function($log,$scope,networkCallService){
+	console.log("MasterCtrl loaded...");
+
+
+})
+.controller('DeviceCtrl', function($log,$scope,networkCallService){
+	console.log("DeviceCtrl loaded...");
+
+});
